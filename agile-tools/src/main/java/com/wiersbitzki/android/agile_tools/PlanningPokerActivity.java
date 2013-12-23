0c9c1f0023ceb460a8dd7902ca47b158140edbca
Julian Wiersbitzki <julian@wiersbitzki.com>
2013-12-23 10:33:51 +0100

    * Pokercards can be swiped via ViewPager

--- agile-tools/src/main/java/com/wiersbitzki/android/agile_tools/PlanningPokerActivity.java
+++ agile-tools/src/main/java/com/wiersbitzki/android/agile_tools/PlanningPokerActivity.java
@@ -2,44 +2,87 @@ package com.wiersbitzki.android.agile_tools;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 public class PlanningPokerActivity extends Activity {
 
 	private static String DIRECTORY = "planningpoker/beige";
-	private String[] files;
+	private List<String> files;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		initializeActivity();
 		setContentView(R.layout.planning_poker);
-		showPokercard(0);
-	}
 
-	private void initializeActivity() {
 		try {
-			files = getAssets().list(DIRECTORY);
-			Log.d(PlanningPokerActivity.class.getSimpleName(), "found " + files.length + " files");
+			files = Arrays.asList(getAssets().list(DIRECTORY));
+			Log.d(PlanningPokerActivity.class.getSimpleName(), "found " + files.size() + " files");
+			Collections.sort(files);
 		} catch (IOException e) {
 			e.printStackTrace();
-			files = new String[0];
+			files = new LinkedList<String>();
 		}
+		ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+		ImagePagerAdapter adapter = new ImagePagerAdapter();
+		viewPager.setAdapter(adapter);
 	}
 
-	private void showPokercard(int index) {
-		InputStream in;
-		ImageView i = (ImageView) findViewById(R.id.imageview_pokercard);
-		try {
-			in = getAssets().open(DIRECTORY + "/" + files[index]);
-			i.setImageDrawable(Drawable.createFromStream(in, null));
-		} catch (IOException e) {
-			e.printStackTrace();
+	private class ImagePagerAdapter extends PagerAdapter {
+
+		private List<Drawable> pokercards = new ArrayList<Drawable>(files.size());
+
+		public ImagePagerAdapter() {
+			InputStream in;
+			for (String file : files) {
+				try {
+					in = getAssets().open(DIRECTORY + "/" + file);
+					pokercards.add(Drawable.createFromStream(in, null));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return pokercards.size();
+		}
+
+		@Override
+		public boolean isViewFromObject(View view, Object object) {
+			return view == ((ImageView) object);
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position) {
+			Context context = PlanningPokerActivity.this;
+			ImageView imageView = new ImageView(context);
+			int padding = context.getResources().getDimensionPixelSize(R.dimen.padding_medium);
+			imageView.setPadding(padding, padding, padding, padding);
+			imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+			imageView.setImageDrawable(pokercards.get(position));
+			((ViewPager) container).addView(imageView, 0);
+			return imageView;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object) {
+			((ViewPager) container).removeView((ImageView) object);
 		}
 	}
 }
